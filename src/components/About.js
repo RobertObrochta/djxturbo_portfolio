4cfcb3ab4ff10eb22d4043ef065ce9e1f401cc07
Robert Obrochta <obrochtarobert13@gmail.com>
2025-12-30 17:15:27 -0600

    added carousel with videos in about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,29 @@
+import VideoEmbed from "./VideoEmbed";
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+
 const About = () => {
   return (
     <div className="about-section" id="About">
+      <div className="videos">
+        <Splide hasTrack={false}>
+
+          <div className="videos-track">
+            <SplideTrack>
+              <SplideSlide>
+                <VideoEmbed videoId= "ip5AMiJ-ijE"/>
+              </SplideSlide>
+
+              <SplideSlide>
+                <VideoEmbed videoId= "ip5AMiJ-ijE"/>
+              </SplideSlide>
+            </SplideTrack>
+            </div>
+
+          <div className="splide__arrows" />
+        </Splide>
+      </div>
     </div>
   );
-
 }
 
 export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from 'react'
-
 const Header = () => {    
     return (
         <header className='header'>
-            <h1>DJ</h1>    
-            <h1>xTurbo</h1>    
+            <h1>DJxT</h1>    
         </header>
     )
 }
